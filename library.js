6c7bf971d0068d6b0a440a73c0d9e644d39fb50c
reneilfuna <123956661+reneilfuna@users.noreply.github.com>
2025-03-07 17:10:52 +0000

    Add books to library for later testing

--- library.js
+++ library.js
@@ -16,5 +16,12 @@ function Book(title, author, pages, readYet) {
 function addBookToLibrary(title, author, pages, readYet){
    // Take params, create a book and store in array
    const bookToAdd = new Book(title, author, pages, readYet);
-   theLibrary.push = bookToAdd;
+   theLibrary.push(bookToAdd);
 }
+
+addBookToLibrary("Game of Thrones", "George R. Martin", 795, 1)
+addBookToLibrary("Lord of the Rings", "J.R.R. Tolkien", 1216, 0)
+addBookToLibrary("East of Eden", "John Steinbeck", 640, 0)
+addBookToLibrary("Why We Sleep", "Matthew Walker", 368, 1)
+addBookToLibrary("Still Life", "Sarah Winman", 448, 1)
+console.log(theLibrary)

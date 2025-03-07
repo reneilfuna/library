079f84a8c5728396f160c0a0427964ed99748d12
reneilfuna <123956661+reneilfuna@users.noreply.github.com>
2025-03-07 16:58:54 +0000

    Create function to add books to array

--- library.js
+++ library.js
@@ -7,11 +7,14 @@ function Book(title, author, pages, readYet) {
    this.author = author;
    this.pages = pages;
    this.readYet = readYet == true ? "read" : "not read";
+   this.id = crypto.randomUUID();
    this.info = function(){
             console.log(`${this.title} by ${this.author}, ${this.pages} pages & ${this.read}`)
          }
 }
 
-function addBookToLibrary(){
+function addBookToLibrary(title, author, pages, readYet){
    // Take params, create a book and store in array
+   const bookToAdd = new Book(title, author, pages, readYet);
+   theLibrary.push = bookToAdd;
 }
